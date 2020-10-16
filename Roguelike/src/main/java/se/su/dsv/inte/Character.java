643fd7b69197469cd5540ea3ef89138cbcf3e3fd
joakimamark <joakim.amark@gmail.com>
2020-10-16 13:44:33 +0200

    Added tests for null check in Character ctr

--- Roguelike/src/main/java/se/su/dsv/inte/Character.java
+++ Roguelike/src/main/java/se/su/dsv/inte/Character.java
@@ -4,6 +4,7 @@ public class Character {
     private String race;
     private int level;
     private String name;
+    // private Item[] inventory = new Item[getInventorySize()]; // Inventory for player character (move to subclass later)
 
     // Declare throws?
     public Character(String race, int level) {
@@ -13,6 +14,8 @@ public class Character {
     public Character(String race, int level, String name) {
         if (level < 1 || level > 100)
             throw new IllegalArgumentException("Level not in allowed range (1-100)");
+        if (name == null || race == null)
+            throw new IllegalArgumentException("Name or race can not be null");
         this.race = race;
         this.level = level;
         this.name = name;
--- Roguelike/src/test/java/se/su/dsv/inte/CharacterTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/CharacterTest.java
@@ -7,15 +7,19 @@ import static org.junit.Assert.*;
 public class CharacterTest {
     private final String DEFAULT_RACE = "Hobbit";
     private final int DEFAULT_LEVEL = 1;
+    private final String DEFAULT_NAME = "Frodo Baggins";
+
+    // Test Enums? Character(RACES.dwarf, 1, Gimli)
 
     @Test
-    public void testDefaultCharacterCtrSetsRaceAndLevel() {
+    public void testCtrSetsAttributes() {
         // No default constructor currently (level has to be specified)
         // Do we need unique id for each Character?
         // Test character level scaling to player level
-        Character character = new Character(DEFAULT_RACE, DEFAULT_LEVEL);
+        Character character = new Character(DEFAULT_RACE, DEFAULT_LEVEL, DEFAULT_NAME);
         assertEquals(DEFAULT_RACE, character.getRace());
         assertEquals(DEFAULT_LEVEL, character.getLevel());
+        assertEquals(DEFAULT_NAME, character.getName());
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -33,5 +37,17 @@ public class CharacterTest {
         Character character = new Character(DEFAULT_RACE, DEFAULT_LEVEL);
         assertEquals(DEFAULT_RACE, character.getName());
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCtrThrowsIAEIfRaceIsNullButNameIsNotNull() {
+        new Character(null, DEFAULT_LEVEL, DEFAULT_NAME);
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCtrThrowsIAEIfNameIsNull() {
+        new Character(DEFAULT_RACE, DEFAULT_LEVEL, null);
+    }
+
 
 }
