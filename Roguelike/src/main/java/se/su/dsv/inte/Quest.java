3156ec86158615b481a964eafaa7c21128a5527b
joakim <joakim.amark@gmail.com>
2020-10-19 10:02:24 +0200

    Sketch of desired interface for Quest class using TDD

---
+++ Roguelike/src/main/java/se/su/dsv/inte/Quest.java
@@ -0,0 +1,9 @@
+package se.su.dsv.inte;
+
+import java.util.Collection;
+
+public class Quest {
+    public Quest(String title, String description, int requiredLevel, Collection<Object> objectives, Object reward) {
+
+    }
+}
--- Roguelike/src/test/java/se/su/dsv/inte/QuestTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/QuestTest.java
@@ -1,7 +1,17 @@
 package se.su.dsv.inte;
 
+import org.junit.Test;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class QuestTest {
-
+    @Test
+    public void testCreateSimpleDeliverQuest() {
+        // Sketch of desired interface.
+        List<Object> objectives = null;
+        Object reward = null;
+        Quest quest = new Quest("Simple Title", "A short description", 1, objectives, reward);
+    }
 }
