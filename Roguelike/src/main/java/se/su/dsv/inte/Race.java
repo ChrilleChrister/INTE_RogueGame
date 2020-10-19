67a7d31b1672208c855bd1c2776288f80b7c9ee7
joakimamark <joakim.amark@gmail.com>
2020-10-19 12:47:26 +0200

    Refactored RaceTest to check default stats. Refactored Race ctr to create default stats

--- Roguelike/src/main/java/se/su/dsv/inte/Race.java
+++ Roguelike/src/main/java/se/su/dsv/inte/Race.java
@@ -1,16 +1,18 @@
 package se.su.dsv.inte;
 
 public enum Race {
-    HOBBIT ("Hobbit", 5),
-    DWARF ("Dwarf", 3);
+    HOBBIT ("Hobbit", 5, new Stats(20, 2, 4)),
+    DWARF ("Dwarf", 3, new Stats(30, 5, 3));
 
-    Race(String name, int baseMovementSpeed) {
+    Race(String name, int baseMovementSpeed, Stats defaultStats) {
         this.name = name;
         this.baseMovementSpeed = baseMovementSpeed;
+        this.defaultStats = defaultStats;
     }
 
     private String name;
     private int baseMovementSpeed;
+    private Stats defaultStats;
 
     public String getName() {
         return name;
@@ -18,5 +20,9 @@ public enum Race {
 
     public int getBaseMovementSpeed() {
         return baseMovementSpeed;
+    }
+
+    public Stats getDefaultStats() {
+        return defaultStats;
     }
 }
--- Roguelike/src/test/java/se/su/dsv/inte/RaceTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/RaceTest.java
@@ -10,11 +10,17 @@ public class RaceTest {
     public void testHobbitRace() {
         assertEquals("Hobbit", Race.HOBBIT.getName());
         assertEquals(5, Race.HOBBIT.getBaseMovementSpeed());
+        assertEquals(20, Race.HOBBIT.getDefaultStats().getHitPoints());
+        assertEquals(2, Race.HOBBIT.getDefaultStats().getAttackPoints());
+        assertEquals(4, Race.HOBBIT.getDefaultStats().getDefensePoints());
     }
 
     @Test
     public void testDwarfRace() {
         assertEquals("Dwarf", Race.DWARF.getName());
         assertEquals(3, Race.DWARF.getBaseMovementSpeed());
+        assertEquals(30, Race.DWARF.getDefaultStats().getHitPoints());
+        assertEquals(5, Race.DWARF.getDefaultStats().getAttackPoints());
+        assertEquals(3, Race.DWARF.getDefaultStats().getDefensePoints());
     }
 }
