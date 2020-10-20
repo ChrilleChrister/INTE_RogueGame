1e42e4c430365f12884c02385c96f28a911bc77a
joakim <joakim.amark@gmail.com>
2020-10-20 13:02:38 +0200

    Added test and subsequent implementation to ensure that monster name can not be passed null in ctr

--- Roguelike/src/main/java/se/su/dsv/inte/quest/MonsterSlayingObjective.java
+++ Roguelike/src/main/java/se/su/dsv/inte/quest/MonsterSlayingObjective.java
@@ -1,24 +1,26 @@
 package se.su.dsv.inte.quest;
 
 public class MonsterSlayingObjective extends QuestObjective {
-    private final String monsterType;
+    private final String monsterName;
     private final int numberToSlay;
     private int numberSlain;
 
-    public MonsterSlayingObjective(String monsterType, int numberToSlay) {
+    public MonsterSlayingObjective(String monsterName, int numberToSlay) {
+        if (monsterName == null)
+            throw new IllegalArgumentException("Monster name can not be null");
         if (numberToSlay < 1)
             throw new IllegalArgumentException("Number of monsters to slay can not be less than one");
-        this.monsterType = monsterType;
+        this.monsterName = monsterName;
         this.numberToSlay = numberToSlay;
     }
 
     @Override
     public String getStatusMessage() {
-        return numberSlain + "/" + numberToSlay + " " + monsterType + " slain";
+        return numberSlain + "/" + numberToSlay + " " + monsterName + " slain";
     }
 
-    public String getMonsterType() {
-        return monsterType;
+    public String getMonsterName() {
+        return monsterName;
     }
 
     public int getNumberToSlay() {
--- Roguelike/src/test/java/se/su/dsv/inte/quest/MonsterSlayingObjectiveTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/quest/MonsterSlayingObjectiveTest.java
@@ -1,45 +1,48 @@
 package se.su.dsv.inte.quest;
 
 import org.junit.Test;
-import se.su.dsv.inte.quest.MonsterSlayingObjective;
-import se.su.dsv.inte.quest.QuestObjective;
 
 import static org.junit.Assert.*;
 
 public class MonsterSlayingObjectiveTest {
-    private static final String DEFAULT_MONSTER_TYPE = "Wolf"; // Refactor to Race instead, include level range(?)
+    private static final String DEFAULT_MONSTER_NAME = "Wolf";
     private static final int DEFAULT_NUMBER_TO_SLAY = 5;
 
     @Test
     public void testMonsterSlayingObjectiveCtrSetsAttributes() {
-        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_TYPE, DEFAULT_NUMBER_TO_SLAY);
-        assertEquals(DEFAULT_MONSTER_TYPE, objective.getMonsterType());
+        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_NAME, DEFAULT_NUMBER_TO_SLAY);
+        assertEquals(DEFAULT_MONSTER_NAME, objective.getMonsterName());
         assertEquals(DEFAULT_NUMBER_TO_SLAY, objective.getNumberToSlay());
         assertEquals(0, objective.getNumberSlain());
     }
 
     @Test (expected = IllegalArgumentException.class)
     public void testMonsterSlayingObjectiveCtrThrowsIAEIfNumberToSlayIsLessThanOne() {
-        new MonsterSlayingObjective(DEFAULT_MONSTER_TYPE, -1);
+        new MonsterSlayingObjective(DEFAULT_MONSTER_NAME, -1);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testMonsterTypeCanNotBeNull() {
+        new MonsterSlayingObjective(null, DEFAULT_NUMBER_TO_SLAY);
     }
 
     @Test
     public void testStatusMessageIsEmptyForNewQuest() {
-        QuestObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_TYPE, DEFAULT_NUMBER_TO_SLAY);
+        QuestObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_NAME, DEFAULT_NUMBER_TO_SLAY);
         assertEquals("0/5 Wolf slain", objective.getStatusMessage());
     }
 
     @Test
     public void testIncrementNumberSlainUpdatedNumberSlainByOne() {
         // should ideally be tested in combat context; testMonsterSlayedIncrementsNumberSlain
-        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_TYPE, DEFAULT_NUMBER_TO_SLAY);
+        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_NAME, DEFAULT_NUMBER_TO_SLAY);
         objective.incrementNumberSlain();
         assertEquals(1, objective.getNumberSlain());
     }
 
     @Test
     public void testNumberSlainStopsCountingAfterNumberToSlayIsReached() {
-        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_TYPE, DEFAULT_NUMBER_TO_SLAY);
+        MonsterSlayingObjective objective = new MonsterSlayingObjective(DEFAULT_MONSTER_NAME, DEFAULT_NUMBER_TO_SLAY);
         for (int i = 0; i < DEFAULT_NUMBER_TO_SLAY + 1; i++)
             objective.incrementNumberSlain();
         assertEquals(DEFAULT_NUMBER_TO_SLAY, objective.getNumberSlain());
