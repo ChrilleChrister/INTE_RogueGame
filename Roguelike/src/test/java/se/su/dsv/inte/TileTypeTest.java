57f951b3835a24e038445e9eca78d49657f23348
LinneaAronsson <58560292+LinneaAronsson@users.noreply.github.com>
2020-10-23 09:55:12 +0200

    Transversive boolean. Mountain can not walk on but grass and snow can be walked on.

--- Roguelike/src/test/java/se/su/dsv/inte/TileTypeTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/TileTypeTest.java
@@ -10,19 +10,21 @@ public class TileTypeTest {
     public void testTileTypeGrass() {
         assertEquals("Grass", TileType.GRASS.getName());
         assertEquals(2, TileType.GRASS.getTileTypeMovementSpeed());
-
+        assertTrue(TileType.GRASS.getIfTransversive());
     }
+
     @Test
     public void testTileTypeMountain() {
         assertEquals("Mountain", TileType.MOUNTAIN.getName());
         assertEquals(0, TileType.MOUNTAIN.getTileTypeMovementSpeed());
-
+        assertFalse(TileType.MOUNTAIN.getIfTransversive());
     }
+
     @Test
     public void testTileTypeSnow() {
         assertEquals("Snow", TileType.SNOW.getName());
         assertEquals(1, TileType.SNOW.getTileTypeMovementSpeed());
+        assertTrue(TileType.SNOW.getIfTransversive());
 
     }
-
 }
