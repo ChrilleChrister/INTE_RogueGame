f943316b7797cd55ea6058ccfadcc87cb5a2ad5a
LinneaAronsson <58560292+LinneaAronsson@users.noreply.github.com>
2020-10-23 09:55:36 +0200

    Transversive boolean. Mountain can not walk on but grass and snow can be walked on.

--- Roguelike/src/main/java/se/su/dsv/inte/TileType.java
+++ Roguelike/src/main/java/se/su/dsv/inte/TileType.java
@@ -4,17 +4,19 @@ public enum TileType {
 
     //Can grass get snow on it?
     //Can Grass have items? Maybe not snow?
-
-    GRASS ("Grass",  2),
-    MOUNTAIN ("Mountain",  0),
-    SNOW ("Snow", 1 );
+    GRASS ("Grass",  2, true),
+    MOUNTAIN ("Mountain",  0, false), //Can not walk on Mountain
+    SNOW ("Snow", 1, true ); //Moves slower in Snow compared to Grass
 
     private String name;
     private int tileTypeMovementSpeed;
+    private boolean transversive;
 
-    TileType(String name, int tileTypeMovementSpeed) {
+
+    TileType(String name, int tileTypeMovementSpeed, boolean transversive) {
         this.name = name;
         this.tileTypeMovementSpeed = tileTypeMovementSpeed;
+        this.transversive = transversive;
     }
 
     public String getName() {
@@ -24,4 +26,10 @@ public enum TileType {
     public int getTileTypeMovementSpeed() {
         return tileTypeMovementSpeed;
     }
+
+    public boolean getIfTransversive(){
+        return transversive;
+    }
+
+
 }
