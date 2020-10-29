9da56dc7bcf86c84bb38690c9e47d8a568187c6d
LinneaAronsson <58560292+LinneaAronsson@users.noreply.github.com>
2020-10-29 07:59:42 +0100

    Update TileTest.java
    Cleanup

--- Roguelike/src/test/java/se/su/dsv/inte/mapTest/TileTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/mapTest/TileTest.java
@@ -1,6 +1,8 @@
-package se.su.dsv.inte;
+package se.su.dsv.inte.mapTest;
 
 import org.junit.Test;
+import se.su.dsv.inte.map.TileType;
+import se.su.dsv.inte.map.Tile;
 
 import static org.junit.Assert.*;
 
@@ -8,13 +10,11 @@ public class TileTest {
 
     private final int TILE_WIDTH_64 = 64;
     private final int TILE_HEIGHT_64 = 64;
-    private final int X_POSITION_ONE = 1;
-    private final int Y_POSITION_ONE = 1;
+    private final int X_POSITION_ONE = 0;
+    private final int Y_POSITION_ONE = 0;
     private TileType TILE_TYPE_GRASS = TileType.GRASS;
-    private TileType TILE_TYPE_SNOW = TileType.SNOW; //Grass, mountain or snow
-    private TileType TILE_TYPE_MOUNTAIN = TileType.MOUNTAIN; //Grass, mountain or snow
-
-
+    private TileType TILE_TYPE_SNOW = TileType.SNOW;
+    private TileType TILE_TYPE_MOUNTAIN = TileType.MOUNTAIN; 
 
     @Test
     public void testCtrSetsAttributesMountain() {
@@ -24,9 +24,8 @@ public class TileTest {
         assertEquals(X_POSITION_ONE, tile.getxPosition());
         assertEquals(Y_POSITION_ONE, tile.getyPosition());
         assertEquals(TILE_TYPE_MOUNTAIN, tile.getTileType());
-
-
     }
+
     @Test
     public void testCtrSetsAttributesGrass() {
         Tile tile = new Tile(X_POSITION_ONE, Y_POSITION_ONE, TILE_TYPE_GRASS);
@@ -35,8 +34,8 @@ public class TileTest {
         assertEquals(X_POSITION_ONE, tile.getxPosition());
         assertEquals(Y_POSITION_ONE, tile.getyPosition());
         assertEquals(TILE_TYPE_GRASS, tile.getTileType());
-
     }
+
     @Test
     public void testCtrSetsAttributesSnow() {
         Tile tile = new Tile(X_POSITION_ONE, Y_POSITION_ONE, TILE_TYPE_SNOW);
@@ -45,7 +44,5 @@ public class TileTest {
         assertEquals(X_POSITION_ONE, tile.getxPosition());
         assertEquals(Y_POSITION_ONE, tile.getyPosition());
         assertEquals(TILE_TYPE_SNOW, tile.getTileType());
-
     }
-
 }
