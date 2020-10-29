ddcf95f494553dcebadd8a864092dbf81803ce69
LinneaAronsson <58560292+LinneaAronsson@users.noreply.github.com>
2020-10-29 08:43:16 +0100

    Moved TileGrid

--- Roguelike/src/main/java/se/su/dsv/inte/map/TileGrid.java
+++ Roguelike/src/main/java/se/su/dsv/inte/map/TileGrid.java
@@ -1,4 +1,4 @@
-package se.su.dsv.inte;
+package se.su.dsv.inte.map;
 
 import java.util.Random;
 
@@ -51,4 +51,9 @@ public class TileGrid {
             worldOfTiles = null;
         }
     }
+
+    public Tile[][] getWorldOfTiles() {
+        return worldOfTiles;
+    }
+
 }
