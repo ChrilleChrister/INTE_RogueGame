1403f8155d33605f38f810d354392b42aa7a2e34
LinneaAronsson <58560292+LinneaAronsson@users.noreply.github.com>
2020-11-26 14:53:54 +0100

    TestforMendelbrot
    Zoom 4.0 and Index 0

--- Roguelike/src/test/java/se/su/dsv/inte/map/TileGridMandelbrotTest.java
+++ Roguelike/src/test/java/se/su/dsv/inte/map/TileGridMandelbrotTest.java
@@ -7,16 +7,28 @@ import static org.junit.Assert.assertEquals;
 public class TileGridMandelbrotTest extends TileGrid {
     private static final int WORLD_WIDTH = 14;
     private static final int WORLD_HEIGHT = 14;
-    private static final int NUMBER_FOR_INTERVAL = 200;
+    private static final int INDEX_TO_CREATE_MAP_FROM = 0;
+    private static final double ZOOM_IN_ON_MANDELBROT = 4.0;
+
 
     public TileGridMandelbrotTest() {
         super(WORLD_WIDTH, WORLD_HEIGHT);
     }
 
+    /**
+     * Emulate random number generator to always return 0 and 4.0 (Corresponding to index 0 and zoom 4.0 in Mandelbrot Fraction).
+     *
+     */
     @Override
-    protected int generateMaxNumberForIterationInMandelbrot() {
-        return NUMBER_FOR_INTERVAL;
+    protected int indexOfMapCreation() {
+        return INDEX_TO_CREATE_MAP_FROM;
     }
+
+    @Override
+    protected double zoomOfMandelbrot() {
+        return ZOOM_IN_ON_MANDELBROT;
+    }
+
 
     @Test
     public void testMandelbrot() {
@@ -26,15 +38,15 @@ public class TileGridMandelbrotTest extends TileGrid {
         assertEquals(TileType.MOUNTAIN, grid[0][0].getTileType());
         assertEquals(TileType.MOUNTAIN, grid[0][1].getTileType());
         assertEquals(TileType.MOUNTAIN, grid[0][2].getTileType());
-        assertEquals(TileType.MOUNTAIN, grid[0][3].getTileType());
-        assertEquals(TileType.MOUNTAIN, grid[0][4].getTileType());
-        assertEquals(TileType.MOUNTAIN, grid[0][10].getTileType());
-        assertEquals(TileType.MOUNTAIN, grid[0][13].getTileType());
+        assertEquals(TileType.SNOW, grid[0][3].getTileType());
+        assertEquals(TileType.SNOW, grid[0][4].getTileType());
+        assertEquals(TileType.SNOW, grid[0][5].getTileType());
+        assertEquals(TileType.SNOW, grid[0][6].getTileType());
         assertEquals(TileType.GRASS, grid[1][0].getTileType());
-        assertEquals(TileType.GRASS, grid[2][0].getTileType());
+        assertEquals(TileType.MOUNTAIN, grid[2][0].getTileType());
         assertEquals(TileType.GRASS, grid[3][0].getTileType());
-        assertEquals(TileType.GRASS, grid[4][0].getTileType());
-        assertEquals(TileType.GRASS, grid[5][0].getTileType());
+        assertEquals(TileType.MOUNTAIN, grid[4][0].getTileType());
+        assertEquals(TileType.SNOW, grid[12][6].getTileType());
 
     }
 }
